f7819873a53b2e8d5b63e032d77883dde9db8a7e
rarcilla <rlarcilla@hotmail.com>
2020-09-30 16:04:26 -0700

    Initial Commit

---
+++ ViewModelsVsModelsTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ViewModelsVsModelsTutorial
+//
+//  Created by Regina Arcilla on 2020-09-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
